3b613a4c4ca7a46901ca8fc3644d69f650d68167
QD1221 <zxcvbnm12122000@gmail.com>
2021-06-08 08:33:16 +0700

    4th commit

--- lib/shopping_cart_page.dart
+++ lib/shopping_cart_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'model/cloth.dart';
+
 class ShoppingCartPage extends StatefulWidget {
   const ShoppingCartPage({Key key}) : super(key: key);
 
@@ -8,9 +10,11 @@ class ShoppingCartPage extends StatefulWidget {
 }
 
 class _ShoppingCartPageState extends State<ShoppingCartPage> {
+  TextEditingController _textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
         centerTitle: true,
@@ -32,7 +36,7 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
                 child: Padding(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: ListView.builder(
-                    itemCount: 4,
+                    itemCount: clothItems.length,
                     itemBuilder: (context, index) {
                       return Padding(
                         padding: const EdgeInsets.only(bottom: 8),
@@ -43,68 +47,113 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
                               Container(
                                 height: 84,
                                 width: 84,
-                                decoration: BoxDecoration(color: Colors.white),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(8),
+                                    image: DecorationImage(
+                                        image:
+                                            NetworkImage(clothItems[index].img),
+                                        fit: BoxFit.cover)),
                               ),
-                              
                               Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text('Dream Walker - Jacket'),
-                                        Spacer(),
-                                        IconButton(
-                                            icon: Icon(Icons.clear),
-                                            onPressed: () {})
-                                      ],
-                                    ),
-                                    Text(
-                                      'Size: Extra Large(XL)', style: TextStyle(
-                                      color: Colors.grey
-                                    ),
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text('\$0.99'),
-                                       Text('\$59.99', style: TextStyle(
-                                         decoration: TextDecoration.lineThrough,
-                                         color: Colors.grey
-                                       ),),
-                                        Spacer(),
-                                        Container(
-                                          height: 24,
-                                          width: 24,
-                                          decoration: BoxDecoration(
-                                            color: Colors.white,
-                                            border: Border.all(
-                                              color: Colors.grey
-                                            ),
-                                            shape: BoxShape.circle
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 12),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Text(
+                                            '${clothItems[index].name}',
+                                            style: TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold),
                                           ),
-                                          child: Icon(Icons.remove, size: 16,),
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.symmetric(horizontal: 12),
-                                          child: Text(
-                                            '01'
+                                          Spacer(),
+                                          IconButton(
+                                              icon: Icon(Icons.clear),
+                                              onPressed: () {})
+                                        ],
+                                      ),
+                                      Text(
+                                        'Size: ${clothItems[index].size}',
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            '\$${clothItems[index].discountPrice}',
+                                            style: TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold),
                                           ),
-                                        ),
-                                        Container(
-                                          height: 24,
-                                          width: 24,
-                                          decoration: BoxDecoration(
-                                              color: Colors.indigo,
-                                              border: Border.all(
-                                                  color: Colors.grey
+                                          SizedBox(
+                                            width: 8,
+                                          ),
+                                          Text(
+                                            '\$${clothItems[index].originPrice}',
+                                            style: TextStyle(
+                                                decoration:
+                                                    TextDecoration.lineThrough,
+                                                color: Colors.grey),
+                                          ),
+                                          Spacer(),
+                                          InkWell(
+                                            onTap: (){
+                                              setState(() {
+                                                clothItems[index].count -= 1;
+                                              });
+                                              if(clothItems[index].count < 2){
+                                                clothItems[index].count = 1;
+                                              }
+                                            },
+                                            child: Container(
+                                              height: 24,
+                                              width: 24,
+                                              decoration: BoxDecoration(
+                                                  color: Colors.white,
+                                                  border: Border.all(
+                                                      color: Colors.grey),
+                                                  shape: BoxShape.circle),
+                                              child: Icon(
+                                                Icons.remove,
+                                                size: 16,
                                               ),
-                                              shape: BoxShape.circle
+                                            ),
                                           ),
-                                          child: Icon(Icons.add, size: 16, color: Colors.white,),
-                                        )
-                                      ],
-                                    )
-                                  ],
+                                          Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 12),
+                                            child: Text(
+                                                clothItems[index].count < 10 ? '0${clothItems[index].count}' : '${clothItems[index].count}'
+                                            ),
+                                          ),
+                                          InkWell(
+                                            onTap: () {
+                                              setState(() {
+                                                clothItems[index].count += 1;
+                                              });
+                                            },
+                                            child: Container(
+                                              height: 24,
+                                              width: 24,
+                                              decoration: BoxDecoration(
+                                                  color: Colors.indigo,
+                                                  border: Border.all(
+                                                      color: Colors.grey),
+                                                  shape: BoxShape.circle),
+                                              child: Icon(
+                                                Icons.add,
+                                                size: 16,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
                                 ),
                               )
                             ],
@@ -125,9 +174,15 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Promo Code',
-                        style: TextStyle(color: Colors.grey),
+                      Expanded(
+                        child: TextField(
+                          controller: _textEditingController,
+                          decoration: InputDecoration(
+                            hintText: 'Promo Code',
+                            border: InputBorder.none
+                          ),
+                          style: TextStyle(color: Colors.grey),
+                        ),
                       ),
                       Container(
                         margin: EdgeInsets.symmetric(vertical: 12),
@@ -150,18 +205,101 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
                 flex: 1,
               ),
               Expanded(
-                child: Placeholder(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  child: Column(
+                    children: [
+                      Spacer(),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Subtotal: ',
+                            style: TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                          Spacer(),
+                          Text(
+                            '\$${clothItems.map((e) => e.count * e.discountPrice).toList().reduce((value, element) => value + element).toStringAsFixed(2)}',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 4,
+                          ),
+                          Text('USD',
+                              style: TextStyle(fontSize: 14, color: Colors.grey))
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Shipping: ',
+                              style: TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                            Spacer(),
+                            Text(
+                              '\$3.78',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text('USD',
+                                style: TextStyle(fontSize: 14, color: Colors.grey))
+                          ],
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        height: 32,
+                      ),
+
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Bag total: ',
+                            style: TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                          Spacer(),
+                          Text(
+                            '\$${(clothItems.map((e) => e.count * e.discountPrice).toList().reduce((value, element) => value + element) + 3.78).toStringAsFixed(2)}',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 4,
+                          ),
+                          Text('USD',
+                              style: TextStyle(fontSize: 14, color: Colors.grey))
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      )
+                    ],
+                  ),
+                ),
                 flex: 3,
               ),
               Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.indigo,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: Center(
-                    child: Text(
-                      'Proceed To Checkout',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                child: InkWell(
+                  onTap: (){
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Completed')));
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.indigo,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Center(
+                      child: Text(
+                        'Proceed To Checkout',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
                     ),
                   ),
                 ),
